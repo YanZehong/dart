72d816d609d368baa1225c4b67d0242e4c4c487e
YanZehong <yanzehong1101@outlook.com>
2022-12-18 21:26:26 +0000

    fix an issue of pytorchlightning ddp

--- models/modelmodules/dasc_module.py
+++ models/modelmodules/dasc_module.py
@@ -14,7 +14,7 @@ from torchmetrics import MaxMetric, MeanMetric, F1Score, Accuracy, Precision
 from collections import OrderedDict
 from torch.optim import AdamW
 from torch.optim.lr_scheduler import ReduceLROnPlateau
-
+import json
 from utils.logger import get_logger
 
 log = get_logger(__name__)
@@ -31,14 +31,14 @@ class DASCModule(pl.LightningModule):
         self.net = build_model(conf=conf)
         self.criterion = torch.nn.CrossEntropyLoss()
         # metric objects for calculating and averaging accuracy across batches
-        self.train_acc = Accuracy(task = "binary")
-        self.val_acc = Accuracy(task = "binary")
-        self.test_acc = Accuracy(task = "binary")
+        self.train_acc = Accuracy(task="multiclass", num_classes=self.conf.model.num_class)
+        self.val_acc = Accuracy(task="multiclass", num_classes=self.conf.model.num_class)
+        self.test_acc = Accuracy(task="multiclass", num_classes=self.conf.model.num_class)
 
         # for averaging f1 across batches
-        self.train_f1 = F1Score(task = "binary", average="macro")
-        self.val_f1 = F1Score(task = "binary", average="macro")
-        self.test_f1 = F1Score(task = "binary", average="macro")
+        self.train_f1 = F1Score(task="multiclass", num_classes=self.conf.model.num_class, average="macro")
+        self.val_f1 = F1Score(task="multiclass", num_classes=self.conf.model.num_class, average="macro")
+        self.test_f1 = F1Score(task="multiclass", num_classes=self.conf.model.num_class, average="macro")
 
         # for averaging loss across batches
         self.train_loss = MeanMetric()
@@ -48,6 +48,11 @@ class DASCModule(pl.LightningModule):
         # for tracking best so far validation accuracy
         self.val_acc_best = MaxMetric()
         self.val_f1_best = MaxMetric()
+
+        # for saving prediction results
+        self.test_preds = []
+        self.test_targets = []
+        self.test_aspects = []
 
     def on_train_start(self):
         # by default lightning executes validation step sanity checks before training starts,
@@ -78,7 +83,8 @@ class DASCModule(pl.LightningModule):
 
     def training_epoch_end(self, outputs):
         # `outputs` is a list of dicts returned from `training_step()`
-        log.info(f"train/loss: {self.train_loss.compute().item()}")
+        current_lr = self.trainer.optimizers[0].param_groups[0]['lr']
+        log.info(f"epoch: {self.current_epoch}, lr: {current_lr :.6f}, train/loss: {self.train_loss.compute().item() :.4f}")
         
 
     def validation_step(self, batch, batch_idx):
@@ -92,8 +98,8 @@ class DASCModule(pl.LightningModule):
         self.val_acc(preds, targets)
         self.val_f1(preds, targets)
         self.log("val/loss", self.val_loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
-        self.log("val/acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("val/f1", self.val_f1, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("val/acc", self.val_acc, on_step=False, on_epoch=True, prog_bar=False, sync_dist=True)
+        self.log("val/f1", self.val_f1, on_step=False, on_epoch=True, prog_bar=False, sync_dist=True)
 
         return {"loss": loss, "preds": preds, "targets": targets}
 
@@ -104,8 +110,12 @@ class DASCModule(pl.LightningModule):
         self.val_f1_best(f1)  # update best so far val f1
         # log `val_acc_best` as a value through `.compute()` method, instead of as a metric object
         # otherwise metric would be reset by lightning after each epoch
-        self.log("val/acc_best", self.val_acc_best.compute(), prog_bar=True)
-        self.log("val/f1_best", self.val_f1_best.compute(), prog_bar=True)
+        self.log("val/acc_best", self.val_acc_best.compute(), prog_bar=True, sync_dist=True)
+        self.log("val/f1_best", self.val_f1_best.compute(), prog_bar=True, sync_dist=True)
+        log.info("")
+        log.info(f"val/loss: {self.val_loss.compute().item() :.4f}")
+        log.info(f"val/acc: {self.val_acc.compute().item() :.4f}")
+        log.info(f"val/f1: {self.val_f1.compute().item() :.4f}")
         
 
     def test_step(self, batch, batch_idx):
@@ -119,13 +129,29 @@ class DASCModule(pl.LightningModule):
         self.test_acc(preds, targets)
         self.test_f1(preds, targets)
         self.log("test/loss", self.test_loss, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
-        self.log("test/acc", self.test_acc, on_step=False, on_epoch=True, prog_bar=True)
-        self.log("test/f1", self.test_f1, on_step=False, on_epoch=True, prog_bar=True)
+        self.log("test/acc", self.test_acc, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
+        self.log("test/f1", self.test_f1, on_step=False, on_epoch=True, prog_bar=True, sync_dist=True)
 
-        return {"loss": loss, "preds": preds, "targets": targets}
+        # update predictions
+        self.test_preds.extend(preds.cpu().numpy().tolist())
+        self.test_targets.extend(targets.cpu().numpy().tolist())
+        self.test_aspects.extend(batch["aspect_ids"].cpu().numpy().tolist())
+
+        return {"loss": loss, "preds": preds, "targets": targets, "aspect_ids": batch["aspect_ids"]}
     
     def test_epoch_end(self, outputs):
-        pass
+        log.info('')
+        log.info(f"test/acc: {self.test_acc.compute().item() :.4f}")
+        log.info(f"test/f1: {self.test_f1.compute().item() :.4f}")
+        
+        output_path = self.conf.output_dir + "/preds-" + str(self.conf.model.arch) + '-' + str(self.conf.data.name) + ".json"
+        with open(output_path, "w") as fw:
+            json.dump(
+                {
+                    "preds": self.test_preds,
+                    "label_ids": self.test_targets,
+                    "aspect_ids": self.test_aspects,
+                }, fw)
 
     def configure_optimizers(self):
         train_conf = self.conf.train
@@ -146,6 +172,8 @@ class DASCModule(pl.LightningModule):
             optimizer_list,
             {
                 "scheduler": scheduler,
-                "monitor": "train/loss",
+                "monitor": "val/loss",
+                "interval": "epoch",
+                "frequency": 1,
             },
         )
--- run.py
+++ run.py
@@ -1,12 +1,13 @@
 import os
 import hydra
+import torch
 from models import build_module
 from datamodules import build_datamodule
 from utils.hydra import replace_reference, fix_relative_path
 import pytorch_lightning as pl
 from pytorch_lightning.utilities.rank_zero import _get_rank
 from pytorch_lightning.loggers import CSVLogger
-from pytorch_lightning.callbacks import RichProgressBar, RichModelSummary, ModelCheckpoint, EarlyStopping
+from pytorch_lightning.callbacks import RichProgressBar, RichModelSummary, ModelCheckpoint, EarlyStopping, LearningRateMonitor
 
 @hydra.main(config_path="./configs", config_name="cfg", version_base="1.12")
 def main(conf):
@@ -38,7 +39,7 @@ def main(conf):
         EarlyStopping(patience=conf.train.patience,
                       monitor=conf.test.monitor,
                       mode=decision_mode,
-                      check_on_train_epoch_end=False)
+                      check_on_train_epoch_end=False),
     ]
 
     conf.gpu = [conf.gpu] if isinstance(conf.gpu, int) else conf.gpu
@@ -46,7 +47,7 @@ def main(conf):
         max_epochs=conf.train.num_epoch,
         callbacks=callbacks,
         gradient_clip_val=conf.train.clip_grad,
-        accelerator='gpu',
+        accelerator="gpu",
         devices=conf.gpu,
         overfit_batches=conf.overfit_batches,
         val_check_interval=conf.val_check_interval,
@@ -54,9 +55,18 @@ def main(conf):
         deterministic=conf.deterministic,
         strategy="ddp" if len(conf.gpu) > 1 else None,
     )
-
+    
     trainer.fit(model=module, datamodule=datamodule)
-    trainer.test(model=module, datamodule=datamodule, ckpt_path="best")
+
+    if len(conf.gpu) > 1:
+        best_ckpt_path = callbacks[2].best_model_path
+        torch.distributed.destroy_process_group()
+        if trainer.is_global_zero:
+            trainer = pl.Trainer(accelerator="gpu", devices=conf.gpu[0])
+            # model = MyModel.load_from_checkpoint(best_ckpt_path)
+            trainer.test(model=module, datamodule=datamodule, ckpt_path=best_ckpt_path)
+    else:
+        trainer.test(model=module, datamodule=datamodule, ckpt_path="best")
 
 if __name__ == '__main__':
     main()
